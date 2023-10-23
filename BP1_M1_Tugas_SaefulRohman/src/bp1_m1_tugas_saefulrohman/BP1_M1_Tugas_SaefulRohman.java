3e47e68a07a8694167d55bd5331649a7a2504fc4
Saeful Rohman <Saefulgaming72@gmail.com>
2023-10-24 02:07:12 +0700

    first commit

---
+++ BP1_M1_Tugas_SaefulRohman/src/bp1_m1_tugas_saefulrohman/BP1_M1_Tugas_SaefulRohman.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bp1_m1_tugas_saefulrohman;
+
+/**
+ *
+ * @author user
+ */
+public class BP1_M1_Tugas_SaefulRohman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
